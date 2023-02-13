28e74be982769ffe74c9bd1f52bcd39aebc4f13f
iso <ismailkurtlojistik@gmail.com>
2023-02-13 21:21:08 +0300

    templete

--- students/views.py
+++ students/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+
+    context = {
+        'title': 'Clarusway',
+        'desc': 'This is a description',
+        'number': 1111,
+        'list1': ['a', 1, 'b', ['c', 2]],
+        'dict1': {
+            'key1': 'value1',
+            'key2': 'value2'
+        }
+
+    }
+
+    return render(request, 'students/home.html', context)
+
+    # return HttpResponse('<p> Hello </p>')
+
+
+'''
+{{ variables }}
+{% command %}
+'''
